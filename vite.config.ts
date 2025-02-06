8af1fe9dada4a5ab3a912f619e01a76d7ec460d7
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-02-06 14:22:34 -0500

    Modify base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
@@ -12,4 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: '/gh-search-vue/',
 });
